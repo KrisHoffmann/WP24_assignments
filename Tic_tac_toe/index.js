a5cc636d427e73d44c9f886e92f638f9af9d7ea1
Krishoffmann <kris@kuiperonline.nl>
2024-06-05 13:23:54 +0200

    made it possible to join lobbies, if after 5 sec no opponent found error will be displayed

--- Tic_tac_toe/index.js
+++ Tic_tac_toe/index.js
@@ -11,39 +11,57 @@ const io= new Server(server)
 
 app.use(express.static(path.resolve("")))
 
-let arr=[]
-let playingArray=[]
+let arr = [];
+let playingArray = [];
 
-io.on("connection",(socket)=>{
-    socket.on("find",(e)=>{
-        if(e.name!=null){
-            arr.push(e.name)
-            if(arr.length>=2){
-                let p1obj={
-                    p1name:arr[0],
-                    p1value:"X",
-                    p1move:""
-                }
-                
-                let p2obj={
-                    p2name:arr[1],
-                    p2value:"O",
-                    p2move:""
-                }
-
-                let obj={
-                    p1:p1obj,
-                    p2:p2obj
-                }
-                playingArray.push(obj)
-
-                arr.splice(0,2)
-
-                io.emit("find",{allPlayers:playingArray})
+io.on("connection", (socket) => {
+    console.log("Client connected");
+  
+    socket.on("find", (e) => {
+      console.log("Find event received", e);
+  
+      if (e.name != null) {
+        arr.push({ socket, name: e.name });
+        if (arr.length >= 2) {
+          console.log("Match found, emitting find event to both clients");
+  
+          let p1 = arr[0];
+          let p2 = arr[1];
+  
+          let p1obj = {
+            p1name: p1.name,
+            p1value: "X",
+            p1move: "",
+          };
+  
+          let p2obj = {
+            p2name: p2.name,
+            p2value: "O",
+            p2move: "",
+          };
+  
+          let obj = {
+            p1: p1obj,
+            p2: p2obj,
+          };
+          playingArray.push(obj);
+  
+          p1.socket.emit("find", { allPlayers: [obj], opponent: p2.name, value: "X" });
+          p2.socket.emit("find", { allPlayers: [obj], opponent: p1.name, value: "O" });
+  
+          arr.splice(0, 2);
+        } else {
+          console.log("Waiting for second player to join");
+          setTimeout(() => {
+            if (arr.length === 1) {
+              console.log("No second player found, emitting find event to single player");
+              socket.emit("find", { message: "No opponent found" });
             }
+          }, 5000); // wait for 5 seconds
         }
-    })
-})
+      }
+    });
+  });
 
 app.get("/",(req,res)=>{
     return res.sendFile("index.html")
