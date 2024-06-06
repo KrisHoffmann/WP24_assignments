daed330127827237952cd672c079ab4b679c1125
Reinout Vrielink <reinoutvrielink@gmail.com>
2024-06-06 18:27:11 +0200

    Added comments to game.php and index.php

--- TicTacToe/game.php
+++ TicTacToe/game.php
@@ -3,18 +3,20 @@ session_start();
 
 $filename = 'data/game_state.json';
 
+// resetting the game if reset button is clicked
 if (isset($_POST['reset'])) {
     $startPlayer = $_POST['startPlayer'] ?? 'X'; // default starting player to 'X' if not provided
     resetGameState($filename, $startPlayer);
     echo json_encode(['status' => 'reset']);
     exit;
 }
-
+// handling moves made by players
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['index']) && isset($_POST['player'])) {
     $index = $_POST['index'];
     $player = $_POST['player'];
 
     $gameState = getGameState($filename);
+    // checking if the cell is empty and if it is the turn of the current player
     if ($gameState['board'][$index] === '' && $gameState['currentPlayer'] === $player) {
         $gameState['board'][$index] = $player;
         $gameState = checkGameState($gameState, $player);
@@ -28,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $gameState = getGameState($filename);
     echo json_encode($gameState);
 }
-
+// retrieving game state
 function getGameState($filename) {
     if (!file_exists($filename)) {
         resetGameState($filename, 'X');
@@ -40,6 +42,7 @@ function saveGameState($filename, $gameState) {
     file_put_contents($filename, json_encode($gameState));
 }
 
+// function to reset game state
 function resetGameState($filename, $startPlayer) {
     $initialState = [
         'board' => array_fill(0, 9, ''),
@@ -49,7 +52,7 @@ function resetGameState($filename, $startPlayer) {
     ];
     saveGameState($filename, $initialState);
 }
-
+// function to check if theres a winner or if its a draw
 function checkGameState($gameState, $player) {
     if (checkWinner($gameState['board'], $player)) {
         $gameState['winner'] = $player;
@@ -59,6 +62,7 @@ function checkGameState($gameState, $player) {
     return $gameState;
 }
 
+// checking for winning combination
 function checkWinner($board, $player) {
     $winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
@@ -77,6 +81,7 @@ function checkWinner($board, $player) {
     return false;
 }
 
+// function to check if the game is a draw
 function checkDraw($board) {
     foreach ($board as $cell) {
         if ($cell === '') {
--- TicTacToe/index.php
+++ TicTacToe/index.php
@@ -9,11 +9,13 @@ include __DIR__ . '/tpl/background.php';
 
 <div id="selection-container">
     <form id="player-selection-form" action="game_screen.php" method="GET">
+        <!-- Button for choosing to be player X -->
         <label>
             <input type="radio" name="player" value="X" required>
             Player 1 (X)
         </label>
         <br>
+        <!-- Button for choosing to be player O -->
         <label>
             <input type="radio" name="player" value="O" required>
             Player 2 (O)
